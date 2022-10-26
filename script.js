61a6d0f69af74e4817a16a1ebd5f9fa02e76c542
Tyler Walton <tylerwalton300@gmail.com>
2022-10-25 22:57:20 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// create a prompt that asks for the length of password
+
+
+
+
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
