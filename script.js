1b451c759d8251d508dcb66a864f2e0e341e28a6
Tyler Walton <tylerwalton300@gmail.com>
2022-10-26 20:52:38 -0400

    adding in arrays and window messages

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 // create a prompt that asks for the length of password
 
+// promt user to pick what types of charecters they want
 
+// make charecter array
+
+var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
+var upperCase= ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var specialChars= ["!@#$%^&*?+-=_'(),.;:<>\{}[]/|`~ "];
+var numbers= ["1234567890"];
+
+//make randomizer equation then put in function 
+
+// make window messages 
+window.alert("HI! Lets make a password")
+
+var addUpperCase = window.prompt ("Would you like to add upper case letters?")
+var addLowerCase = window.prompt ("Would you like to add lower case letters?")
+var numberOfChar = window.prompt ("How many charecters would you like to ad? You can choose between 8 & 128.")
+var addSpecialChar = window.prompt ("How about some special charecters?")
+var addNumbers = window.prompt ("Lastly, would you like to add some numbers?")
+
+// combine arrays and put into a function
 
 
 
@@ -17,4 +37,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click writePassword);
