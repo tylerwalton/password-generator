2010cc4ee8894243ce5961355368170fda91344c
Tyler Walton <tylerwalton300@gmail.com>
2022-10-28 19:28:58 -0400

    Updated code with arrays and equation

--- script.js
+++ script.js
@@ -1,28 +1,61 @@
-// create a prompt that asks for the length of password
+// make character array
 
-// promt user to pick what types of charecters they want
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var specialChars= "!@#$%^&*?+-=_'(),.;:<>{}[]/|`~ ";
+var numbers= "1234567890";
 
-// make charecter array
 
-var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
-var upperCase= ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-var specialChars= ["!@#$%^&*?+-=_'(),.;:<>\{}[]/|`~ "];
-var numbers= ["1234567890"];
+var charecterPool = [];
+var password = [];
+// combine arrays and put into a function
+var generatePassword = function (){
+  lowerCase = lowerCase.split("");
+  upperCase = upperCase.split("");
+  specialChars = specialChars.split("");
+  numbers = numbers.split("");
 
-//make randomizer equation then put in function 
-
-// make window messages 
+  // make window messages 
 window.alert("HI! Lets make a password")
 
-var addUpperCase = window.prompt ("Would you like to add upper case letters?")
-var addLowerCase = window.prompt ("Would you like to add lower case letters?")
-var numberOfChar = window.prompt ("How many charecters would you like to ad? You can choose between 8 & 128.")
-var addSpecialChar = window.prompt ("How about some special charecters?")
-var addNumbers = window.prompt ("Lastly, would you like to add some numbers?")
+var numberOfChar = window.prompt ("How many charecters would you like to add?")
+if (numberOfChar < 8 || numberOfChar > 128) {
+  alert("Passwrod Length must be between 8 and 128")
+  return;
+}
 
-// combine arrays and put into a function
+var addUpperCase = window.confirm ("Would you like to add upper case letters?")
+var addLowerCase = window.confirm ("Would you like to add lower case letters?")
+var addSpecialChar = window.confirm ("How about some special charecters?")
+var addNumbers = window.confirm ("Lastly, would you like to add some numbers?")
+if (!addUpperCase && !addLowerCase && !addSpecialChar && !addNumbers) {
+  alert("You must choose at least one charecter type.")
+  return;
+}
 
+if ( addUpperCase === true ) {
+  charecterPool = charecterPool.concat (upperCase)
+}
+if ( addLowerCase === true ) {
+  charecterPool = charecterPool.concat (lowerCase)
+}
+if ( addSpecialChar === true ) {
+  charecterPool = charecterPool.concat (specialChars)
+}
+if ( addNumbers === true ) {
+  charecterPool = charecterPool.concat (numbers)
+}
+console.log(charecterPool);
 
+//make randomizer equation then put in function 
+for (var i = 0; i < numberOfChar; i++){
+  var randomIndex = Math.floor (Math.random()* charecterPool.length) 
+var indexValue =  charecterPool [randomIndex]
+password.push(indexValue);
+
+}
+return password.join ('')
+}
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -37,4 +70,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click writePassword);
+generateBtn.addEventListener("click", writePassword);
